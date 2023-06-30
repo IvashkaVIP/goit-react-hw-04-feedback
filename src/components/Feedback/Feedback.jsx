131d72a5231de53f7395f79546bcd264bbffdeb2
IvashkaVIP <ivashka.co@gmail.com>
2023-06-30 22:28:01 +0300

    add step state+onClick

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,29 +1,47 @@
-import React from "react";
-
+import React from 'react';
 
 export class Feedback extends React.Component {
-  handleGoodBtn() { console.log('good')}
-  handleNeutralBtn() {console.log('neutral')}
-  handleBadBtn() {console.log('bad')}
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleGoodBtn = () => {
+      this.setState({ good: 10 });
+      console.log('good');
+  };
+    handleNeutralBtn = () => {
+      this.setState({ neutral: 20 });
+    console.log('neutral');
+  };
+    handleBadBtn = () => {
+      this.setState({ bad: 30 });
+    console.log('bad');
+  };
 
   render() {
     return (
       <>
         <h2>Please leave feedback</h2>
         <div className="btn-wrap">
-          <button type="button" onClick={this.handleGoodBtn}>Good</button>
-          <button type="button" onClick={this.handleNeutralBtn}>Neutral</button>
-          <button type="button" onClick={this.handleBadBtn}>Bad</button>
+          <button type="button" onClick={this.handleGoodBtn}>
+            Good
+          </button>
+          <button type="button" onClick={this.handleNeutralBtn}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleBadBtn}>
+            Bad
+          </button>
         </div>
         <h2>Statistics</h2>
-        <p>Good: </p>
-        <p>Neutral: </p>
-        <p>Bad: </p>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
       </>
     );
   }
 }
-
 
 // export const Feedback = ({ }) => {
 //     return (
