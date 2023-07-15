547ccfa117bb5e815522ea0d4bce9673a72dd67a
IvashkaVIP <ivashka.co@gmail.com>
2023-07-15 15:05:48 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,43 @@
-import { useState } from "react";
-import { Component } from "react";
-import { Section } from "./Section/Section";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Statistics } from "./Statistics/Statistics";
-import { Notification } from "./Notification/Notification";
-import css from './App.module.css'
-
+import { useState } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+import css from './App.module.css';
 
 export const App = () => {
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-
-
-   
-  const onLeaveFeedBack = (evt, btnName) => {
-    console.log('Click on Btn ', btnName);
-    
-    switch (btnName) {
-      case 'good':
-        setGood(good+1);
-        break;
-      case 'neutral':
-        setNeutral(neutral+1);
-        break;
-      case 'bad':
-        setBad(bad+1);
-        break;
-      
-      default: Notification.error('unknown parameter');
-    }
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  
-  // onLeaveFeedBack = (evt, btnName) => {
-  //    this.setState(prevState => ({
-  //      [btnName]: ++prevState[btnName],
-  //    }));
-  //  };
+  const countPositiveFeedbackPercentage = () => {
+    return !total ? '0%' : ((good / total) * 100).toFixed(0) + '%';
+  };
 
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  const onLeaveFeedBack = (evt, btnName) => {
+    // console.log('Click on Btn ', btnName);
+    switch (btnName) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      default:
+        alert('unknown parameter in function onLeaveFeedBack');
+    }
+  };
 
   return (
     <div className={css.container}>
@@ -49,64 +46,21 @@ export const App = () => {
           options={{ good, neutral, bad }}
           onLeaveFeedBack={onLeaveFeedBack}
         />
-        <h1>
-          {good} {neutral} {bad}
-        </h1>
       </Section>
-    </div>
-  );
 
-}
-//-------------------------------------------------------------------------
-export class OldApp extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  onLeaveFeedBack = (evt, btnName) => {
-    this.setState(prevState => ({
-      [btnName]: ++prevState[btnName],
-    }));
-  };
-
-  countTotalFeedback() {
-    const ref = this.state;
-    return ref.good + ref.neutral + ref.bad;
-  }
-  countPositiveFeedbackPercentage() {
-    let total = this.countTotalFeedback();
-    return !total ? '0%' : ((this.state.good / total) * 100).toFixed(0) + '%';
-  }
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <div className={css.container}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.state}
-            onLeaveFeedBack={this.onLeaveFeedBack}
+      {total ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
         </Section>
-
-        {total ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,28 +1,7 @@
 import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css'
 
-
-
-
 export const FeedbackOptions = ({ options, onLeaveFeedBack }) => (
-  <div className={css['btn-wrap']}>
-    {Object.keys(options).map(option => (
-      <button
-        className={css.btn}
-        type="button"
-        key={option}
-        onClick={evt => onLeaveFeedBack(evt, option)}
-      >
-        {option}
-      </button>
-    ))}
-  </div>
-);
-
-
-
-
-export const OldFeedbackOptions = ({ options, onLeaveFeedBack }) => (
   <div className={css['btn-wrap']}>
     {Object.keys(options).map(option => (
       <button
